36ccca619ae61df45891b3be45a769f23d3b1d36
Barış Bükümcüler <baris_bukumculer@hotmail.com>
2023-07-16 21:13:41 +0300

    mimari

---
+++ Business_Layer/Concrete/ContactManager.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ContactManager : IGenericService<Contact>
+    {
+        public void TAdd(Contact t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Contact t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Contact TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Contact> TGetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Contact t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
