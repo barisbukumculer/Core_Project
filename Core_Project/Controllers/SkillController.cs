0e7cb86b1ef42c03572be11a5d11e92132c1c108
Barış Bükümcüler <baris_bukumculer@hotmail.com>
2023-07-18 16:29:34 +0300

    skil crud

--- Core_Project/Controllers/SkillController.cs
+++ Core_Project/Controllers/SkillController.cs
@@ -1,12 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Project.Controllers
 {
     public class SkillController : Controller
     {
+        SkillManager skillManager =new SkillManager(new EfSkillDal());
         public IActionResult Index()
         {
+            ViewBag.v1 = "Yetenek Listesi";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Listesi";
+            var values = skillManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddSkill()
+        {
+            ViewBag.v1 = "Yetenek Ekleme";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Ekleme";
             return View();
+        }
+        [HttpPost]
+        public IActionResult AddSkill(Skill skill)
+        {
+            skillManager.TAdd(skill);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteSkill(int id)
+        {
+            var deleteid = skillManager.TGetByID(id);
+            skillManager.TDelete(deleteid);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateSkill(int id)
+        {
+            ViewBag.v1 = "Yetenek Güncelleme";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Güncelleme";
+            var updateid = skillManager.TGetByID(id);
+            return View(updateid);
+        }
+        [HttpPost]
+        public IActionResult UpdateSkill(Skill skill)
+        {
+            ViewBag.v1 = "Yetenek Güncelleme";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Güncelleme";
+            skillManager.TUpdate(skill);
+            return RedirectToAction("Index");
         }
     }
 }
