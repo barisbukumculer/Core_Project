b2c17c4704ce7ba98078a0b3d44b22de28143a40
Barış Bükümcüler <baris_bukumculer@hotmail.com>
2023-07-25 00:33:00 +0300

    .

---
+++ Core_Project/ViewComponents/Dashboard/AdminNavbarMessageList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.ViewComponents.Dashboard
+{
+    public class AdminNavbarMessageList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
